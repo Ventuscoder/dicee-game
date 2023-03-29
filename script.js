ae7a7aa4aba7bec2ea768efccd7fdd6981507cb8
ventuscoder <shivankcode@gmail.com>
2023-03-29 15:48:12 +0530

    Add button functionality

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let firstDiceValue = Math.floor(Math.random()*6)+1
-let secondDiceValue = Math.floor(Math.random()*6)+1
-
 let heading = document.querySelector('h1')
+let btn = document.querySelector('button')
 let firstDice = document.querySelector('.img1')
 let secondDice = document.querySelector('.img2')
 
-console.log(firstDice.setAttribute('src', `/images/dice${firstDiceValue}.png`))
-console.log(secondDice.setAttribute('src', `/images/dice${secondDiceValue}.png`))
-
-if (firstDiceValue > secondDiceValue) {
-    heading.textContent = '🚩 First Player Wins!'
-} else if (secondDiceValue > firstDiceValue) {
-    heading.textContent = 'Second Player Wins! 🚩'
-} else {
-    heading.textContent = 'Draw, Refresh the page'
-}
+btn.addEventListener('click',e=>{
+    let firstDiceValue = Math.floor(Math.random()*6)+1
+    let secondDiceValue = Math.floor(Math.random()*6)+1
+    firstDice.setAttribute('src', `/images/dice${firstDiceValue}.png`)
+    secondDice.setAttribute('src', `/images/dice${secondDiceValue}.png`)
+    
+    if (firstDiceValue > secondDiceValue) {
+        heading.textContent = '🚩 First Player Wins!'
+    } else if (secondDiceValue > firstDiceValue) {
+        heading.textContent = 'Second Player Wins! 🚩'
+    } else {
+        heading.textContent = 'Draw, Refresh the page'
+    }
+})
